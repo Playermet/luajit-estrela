8ce624bce92f5af2f30d952f79f06b4caef4359e
Playermet <playermet@gmail.com>
2015-08-07 18:43:45 +0300

    Add table methods - getn, keys, vals, copy;

--- est/table.lua
+++ est/table.lua
@@ -1,3 +1,35 @@
 local mod = {}
 
+function mod.getn(t)
+  local r = 0
+  for k,v in pairs(t) do
+    r = r + 1
+  end
+  return r
+end
+
+function mod.keys(t)
+  local r = {}
+  for k in pairs(t) do
+    r[#r + 1] = k
+  end
+  return r
+end
+
+function mod.vals(t)
+  local r = {}
+  for _,v in pairs(t) do
+    r[#r + 1] = v
+  end
+  return r
+end
+
+function mod.copy(t)
+  local r = {}
+  for k,v in pairs(t) do
+    r[k] = v
+  end
+  return r
+end
+
 return mod
