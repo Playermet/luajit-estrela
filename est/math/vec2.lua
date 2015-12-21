52a53303588f332781db860be118fd1991938542
Playermet <playermet@gmail.com>
2015-12-21 21:50:16 +0200

    Update math.vec2 module;

--- est/math/vec2.lua
+++ est/math/vec2.lua
@@ -1,3 +1,13 @@
-local mod = {}
+local oop = require 'est.lang.oop'
+
+local mod = oop.class()
+
+function mod.new(class, x,y)
+  return oop.object(class, { x = x, y = y })
+end
+
+function mod:unpack()
+  return self.x, self.y
+end
 
 return mod
