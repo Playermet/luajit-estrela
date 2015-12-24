f330267a6227589bc19eb495855dba19fcdee266
Playermet <playermet@gmail.com>
2015-12-24 22:54:33 +0200

    Add array methods - find, match;

--- est/array.lua
+++ est/array.lua
@@ -6,8 +6,46 @@ end
 
 function mod.copy(a)
   local r = {}
-  for i = 1,#a do do
+  for i = 1,#a do
     r[i] = a[i]
+  end
+  return r
+end
+
+function mod.find(a, what, from)
+  for i = from,#a do
+    if a[i] == what then
+      return i
+    end
+  end
+  return nil
+end
+
+function mod.find_all(a, what, from)
+  local r = {}
+  for i = from,#a do
+    if a[i] == what then
+      r[#r + 1] = i
+    end
+  end
+  return r
+end
+
+function mod.match(a, match_func, from)
+  for i = from,#a do
+    if match_func(i,a[i],a) then
+      return i
+    end
+  end
+  return nil
+end
+
+function mod.match_all(a, match_func, from)
+  local r = {}
+  for i = from,#a do
+    if match_func(i,a[i],a) then
+      r[#r + 1] = i
+    end
   end
   return r
 end
